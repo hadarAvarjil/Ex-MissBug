5d50108f46733e52bc4ba30725747785bc33b89d
hadar Avarjil <hadaravarjil@gmail.com>
2024-10-30 21:04:11 +0200

    Provide an API for Bugs CRUDL

--- server.js
+++ server.js
@@ -1,4 +1,69 @@
 import express from 'express' 
+import { bugService } from './services/bug.service.js'
+import cookieParser from 'cookie-parser'
+
+
 const app = express() 
+
+app.use(express.static('public'))
+app.use(cookieParser())
+
+//* Express Routing:
+app.get('/api/bug', (req, res) => {
+    bugService.query()
+        .then(bugs => res.send(bugs))
+        .catch(err => {
+            loggerService.error('Cannot get bugs', err)
+            res.status(500).send('Cannot get bugs')
+        })
+})
+
+app.get('/api/bug/save', (req, res) => {
+
+    const bugToSave = {
+        _id: req.query._id,
+        title: req.query.title,
+        severity: +req.query.severity,
+        description: req.query.description
+    }
+
+    bugService.save(bugToSave)
+        .then(savedBug => res.send(savedBug))
+        .catch((err) => {
+            loggerService.error('Cannot save bug', err)
+            res.status(500).send('Cannot save bug', err)
+        })
+})
+
+app.get('/api/bug/:bugId', (req, res) => {
+    const { bugId } = req.params
+    bugService.getById(bugId)
+        .then(bug => res.send(bug))
+        .catch(err => {
+            loggerService.error('Cannot get bug', err)
+            res.status(500).send('Cannot get bug')
+        })
+})
+
+
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    const { bugId } = req.params
+    bugService.remove(bugId)
+        .then(() => res.send(bugId + ' Removed Successfully!'))
+        .catch(err => {
+            loggerService.error('Cannot remove bug', err)
+            res.status(500).send('Cannot remove bug')
+        })
+})
+
+
+// const port = 3030
+// app.listen(port, () =>
+//     loggerService.info(`Server listening on port http://127.0.0.1:${port}/`)
+// )
+
+
+
 app.get('/', (req, res) => res.send('Hello there')) 
 app.listen(3030, () => console.log('Server ready at port 3030')) 
